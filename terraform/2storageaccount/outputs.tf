e07e49288c846a7cef23dbb7483c78d6be630131
jsvirkdevops <jvseroka2020@gmail.com>
2021-04-25 18:37:04 -0400

    added a little advance terraform code

---
+++ terraform/2storageaccount/outputs.tf
@@ -0,0 +1,8 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg1
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa1
+
+}
---
+++ terraform/2storageaccount/variables.tf
@@ -0,0 +1,25 @@
+locals {
+  task_id               = "abcd"
+  azurerm_resource_name = "test${local.task_id}${random_id.random-suffix.dec}"
+}
+
+resource "random_id" "random-suffix" {
+  byte_length = 1
+}
+
+variable "access_tier" {
+  default = "Standard"
+}
+
+variable "location" {
+  default = "eastus2"
+}
+
+resource "random_string" "random-suffix" {
+    length = 4
+    lower = true
+    min_lower = 4
+    number = false
+    special = false
+    upper = false
+}
