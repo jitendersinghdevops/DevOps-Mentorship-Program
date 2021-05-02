98a806d43fb209ec9e483d066d0592be5a34f647
jsvirkdevops <jvseroka2020@gmail.com>
2021-05-02 00:36:37 -0400

    create new files

--- scripts/storageaccount/storageaccount2.ps1
+++ scripts/storageaccount/storageaccount2.ps1
@@ -4,14 +4,16 @@
 .DESCRIPTION
    Create a storage account in azure cloud, this is fairly simple script to learn how script work and learn about the utility of variables.
 .EXAMPLE
-   ./2storageaccount.ps1
+   ./storageaccount2.ps1
 #>
 
 $test_rg = az group create `
-    --location eastus2 `
-    --name abcd-$(Get-Random) | ConvertFrom-Json
+   --location eastus2 `
+   --name abcd-$(Get-Random) | ConvertFrom-Json
 
 $test_storage_account = az storage account create `
     --resource-group $test_rg.name `
     --name testsa$(Get-Random) | ConvertFrom-Json
 
+$test_rg
+$test_storage_account
