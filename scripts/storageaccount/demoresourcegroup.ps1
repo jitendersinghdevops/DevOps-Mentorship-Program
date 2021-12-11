dd140f08e18251cc9cde799e31e2747342095a13
Jitender_singh <jvseroka2020@gmail.com>
2021-12-11 12:13:14 -0500

    thuis is my additional information

--- scripts/storageaccount/demoresourcegroup.ps1
+++ scripts/storageaccount/demoresourcegroup.ps1
@@ -13,3 +13,10 @@ az group list --query "[].{RGN:name, location:location}"
 az group list --query "[?name=='dmprg']"
 
 az group list --query "[?name=='dmprg'].name"
+
+az group list  | ConvertFrom-Json
+
+az group list  -o yaml
+
+
+skjfaklsnafskl
