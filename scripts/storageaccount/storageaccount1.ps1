14d0bbfe890e36b8d1a15ce004354ccf0bcc1e4b
Navjot Kohar <mbfnav@gmail.com>
2021-12-11 14:34:30 -0500

    Update storageaccount1.ps1

--- scripts/storageaccount/storageaccount1.ps1
+++ scripts/storageaccount/storageaccount1.ps1
@@ -16,7 +16,7 @@
 
 $test_rg =  az group create `
          --location eastus2 `
-         --name   NAVYANAVMEETSARMEkjds | ConvertFrom-Json
+         --name   nav1 | ConvertFrom-Json
 
 
 $test_sa = az storage account create --name  jitenderstorageajsfh --resource-group $test_rg.name 
